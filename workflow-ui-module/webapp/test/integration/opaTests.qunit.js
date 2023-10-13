87ec495ea3602ae2114871c6c7d82bf6e3eb8bae
Willem Pardaens <willem.pardaens@sap.com>
2023-10-13 16:37:43 +0000

    final

---
+++ workflow-ui-module/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["workflowuimodule/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ workflow-ui-module/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"workflow-ui-module/test/unit/controller/MyTaskUI.controller"
+], function () {
+	"use strict";
+});
